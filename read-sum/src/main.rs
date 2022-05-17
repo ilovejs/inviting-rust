0c269af008e8a801b0472ca28db902f7d7947d94
ilovejs <zhuangdeyouxiang@gmail.com>
2022-05-17 21:28:35 +1000

    cal

--- read-sum/src/main.rs
+++ read-sum/src/main.rs
@@ -5,9 +5,8 @@ use std::io::prelude::*;
 use std::io;
 use std::num;
 
-
-use thiserror::Error;
 use anyhow::{Context, Result};
+use thiserror::Error;
 
 #[derive(Error, Debug)]
 enum CliError {
@@ -18,27 +17,29 @@ enum CliError {
 }
 
 fn run(filename: &str) -> Result<i32, anyhow::Error> {
-    let mut file = File::open(filename)
-        .context(format!("unable to open '{}'", filename))?;
+    let mut file = File::open(filename).context(format!("unable to open '{}'", filename))?;
 
+    // read into string
     let mut contents = String::new();
     file.read_to_string(&mut contents)
         .context(format!("unable read string '{}'", filename))?;
+
     let mut sum = 0;
-    for c in contents.lines(){
+    for c in contents.lines() {
         let n: i32 = c.parse::<i32>()?;
         sum += n;
     }
+
     Ok(sum)
 }
 
-
+// cargo run a.md
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
 
+    // then
     let res = run(filename)?;
-
     println!("{:?}", res);
 
     Ok(())
